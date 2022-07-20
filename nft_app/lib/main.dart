fbf0dfc00f1c4761ee05a61bca5ecef16e2a4df2
Karan Solanki <karansolanki@orient.com>
2022-07-20 10:40:38 +0530

    test

--- nft_app/lib/main.dart
+++ nft_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
-      title: 'Flutter Demo',
+      title: 'KRAN DEMP',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
